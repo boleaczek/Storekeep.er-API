e1a4c6e315acaaeb3bc274ea5ea85040307fb03b
Kamil R <kamil.r@interia.pl>
2018-04-07 19:21:29 +0200

    added product model and all products endpoint

--- storekeeper.py
+++ storekeeper.py
@@ -1,8 +1,41 @@
-from flask import Flask, url_for
-import sqlite3
-app = Flask(__name__)
-db = sqlite3.connect('data/baza.db')
+from flask import Flask, url_for, jsonify
+from flask_sqlalchemy import SQLAlchemy
 
+
+# inicjalizacja i konfiguracja aplikacji
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data/baza.db'
+db = SQLAlchemy(app)
+
+
+# model produktu
+class Product(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	name = db.Column(db.String(256), nullable=False)
+	desc = db.Column(db.String())
+
+	def __repr__(self):
+		return '<Product %r>' % self.id
+
+	def to_json(self):
+		return {
+			'id': self.id,
+			'name': self.name,
+			'desc': self.desc
+		}
+
+
+# wszystkie produkty
+@app.route('/all')
+def allProducts():
+	return jsonify([p.to_json() for p in Product.query.all()])
+
+
+# strona główna
 @app.route('/')
 def index():
 	return "Hello World"
+	
+
+# uruchomienie aplikacji
+app.run()
