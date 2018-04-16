9e0c030faa4b3831ff3fd38490cb77929333cc13
Kamil R <kamil.r@interia.pl>
2018-04-16 09:57:56 +0200

    updated endpoints and added DETELE one

--- storekeeper.py
+++ storekeeper.py
@@ -30,13 +30,13 @@ class Product(db.Model):
 		self.desc = desc
 
 # wszystkie produkty
-@app.route('/all')
+@app.route('/all', methods=['GET'])
 def showProducts():
 	return jsonify([p.to_json() for p in Product.query.all()]), 200
 
 
 # jeden produkt
-@app.route('/product/<id>')
+@app.route('/product/<id>', methods=['GET'])
 def showProduct(id):
 	product = Product.query.filter_by(id=id).first()
 
@@ -44,22 +44,46 @@ def showProduct(id):
 		return jsonify(product.to_json()), 200
 	else:
 		return jsonify({
-			'msg': 'nie ma takiego produktu'
+			'msg': 'Product not found.'
 		}), 404
 
-@app.route('/product', methods=['POST', 'GET'])
+
+# dodanie nowego produktu
+@app.route('/product', methods=['POST'])
 def addProduct():
-	if request.method == 'POST':
-		product = Product(request.json["name"], request.json["desc"])
-		db.session.add(product)
+	json = request.get_json()
+	product = Product(json.get('name'), json.get('desc'))
+	db.session.add(product)
+	db.session.commit()
+
+	return jsonify({
+		'msg': 'Product added.'	
+	}), 200
+
+
+# usunięcie istniejącego produktu
+@app.route('/product/<id>', methods=['DELETE'])
+def deleteProduct(id):
+	product = Product.query.filter_by(id=id).first()
+
+	if product is not None:
+		db.session.delete(product)
 		db.session.commit()
-		return "Product added", 200
+
+		return jsonify({
+			'msg': 'Product deleted.'
+		}), 200
+	else:
+		return jsonify({
+			'msg': 'Product not found.'
+		}), 404
 
 
 # strona główna
 @app.route('/')
 def index():
-	return "Hello World", 200
+	msg = "Allowed requests: GET /all, GET /product/<id>, POST /product, DELETE /product/<id>"
+	return msg, 200
 	
 
 # uruchomienie aplikacji
